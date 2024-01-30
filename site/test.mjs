f3d8d8d3d04c41118489cef25f4e1bab7c470dce
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-01-30 12:34:04 +0800

    add kbd show/hide in test.html

--- site/test.mjs
+++ site/test.mjs
@@ -1,4 +1,42 @@
 import { Keyboard } from "../src/keyboard"
 
 var kbd = window.kbd = new Keyboard(document)
-kbd.show()
+
+const textarea = document.getElementById("textarea")
+let working = false
+
+function supportsInputModeNone() {
+  return "inputMode" in textarea && !isSafari12OrBelow()
+}
+
+function isSafari12OrBelow() {
+  if (!navigator.userAgent.match("Safari")) return false
+  if (navigator.userAgent.match("Chrome")) return false
+  const m = navigator.userAgent.match(/Version\/(\d+)/)
+  return !!m && parseInt(m[1]) < 13
+}
+
+if (supportsInputModeNone) {
+  textarea.onfocus = function () {
+    kbd.show()
+  }
+} else {
+  textarea.onfocus = textarea.onclick = function () {
+    if (this.readOnly) return
+    working = true
+    this.blur()
+    this.readOnly = true
+    this.focus()
+    setTimeout(() => {
+      this.readOnly = false
+      kbd.show()
+      this.scrollIntoView({ behavior: "smooth" })
+      working = false
+    }, 0)
+  }
+}
+
+textarea.onblur = function () {
+  if (working) return
+  kbd.hide()
+}
